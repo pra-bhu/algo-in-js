c217bece1aaeb18c07b478f0cdbcb742181ca511
Prashant <prashant.bhushan@outlook.com>
2022-02-11 17:47:10 +0530

    adds pop funtionality to Singly Linked List

--- src/ds/SinglyLinkedList.ts
+++ src/ds/SinglyLinkedList.ts
@@ -1,9 +1,9 @@
 class Node {
     value:string
-    next?:Node = null
-    constructor(value: string, next?:Node) {
+    next?:Node
+    constructor(value: string, next?:Node ) {
         this.value = value
-        this.next = next
+        this.next = null
     }
 }
 
@@ -17,7 +17,7 @@ class SinglyLinkedList {
         this._length = 0
     }
 
-    push(value:string){
+    push(value:string): SinglyLinkedList{
         const node = new Node(value)
         if(this._length <= 0){
             this._head = node
@@ -31,30 +31,35 @@ class SinglyLinkedList {
         return this
     }
 
+    pop(): Node{
+        let previousNode = null
+        let currentNode = this._head
+        while(currentNode.next) {
+            previousNode = currentNode
+            currentNode = currentNode.next
+        }
+        previousNode.next = null
+        this._tail = previousNode
+        this._length -= 1
+        return currentNode
+    }
+
     
-    public get length() : number {
+    get length() : number {
         return this._length
     }
 
     
-    public get tail() : Node {
+    get tail() : Node {
         return this._tail
     }
     
     
-    public get head() : Node {
+    get head() : Node {
         return this._head
     }
     
 }
 
-let singlyLinkedList = new SinglyLinkedList()
-singlyLinkedList.push("Node1")
-console.log('tails before insertion',singlyLinkedList.tail)
-singlyLinkedList.push("Node2")
-singlyLinkedList.push("Node3")
-singlyLinkedList.push("Node4")
-// console.log('singlyLinkedList.length : ' + singlyLinkedList.length + ' ')
-console.log('head',singlyLinkedList)
-console.log('tails after insertion',singlyLinkedList.tail)
 
+export { SinglyLinkedList }
