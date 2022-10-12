fa65bfe17465034ffec974d66738816c723829d0
Prashant <prashant.bhushan@outlook.com>
2022-10-12 13:17:48 +0530

    adds pop, shift

--- src/ds/DoublyLinkedList.ts
+++ src/ds/DoublyLinkedList.ts
@@ -37,7 +37,7 @@ class DoublyLinkedList {
         if(this._length <= 0) {
             this._head = node
             this._tail = node
-        }else{
+        }else{   
             this._tail.next = node
             node.prev = this._tail
             this._tail = node
@@ -45,6 +45,38 @@ class DoublyLinkedList {
         this._length++
         return this
     }
+
+    pop(): Node{
+        if (this._length <= 0) return null
+        
+        let oldTail = this._tail
+        if (this._length == 1){
+            this._tail = null
+            this._head = null
+        }else{
+            this._tail = oldTail.prev
+            this._tail.next = null
+            oldTail.prev = undefined
+        }
+        this._length--
+        return oldTail
+    }
+
+    shift(): Node{
+        if (!this._head) return null
+        let oldHead = this._head
+        if (this._length == 1){
+            this._head = null
+            this._tail = null
+        }else{
+            this._head = oldHead.next
+            this._head.prev = null
+            oldHead.next = undefined
+        }
+        this._length--
+        return oldHead
+    }
+
 }
 
 export { DoublyLinkedList }
