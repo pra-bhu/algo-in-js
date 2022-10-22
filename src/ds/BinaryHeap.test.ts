95d7216404f5ae147a7fcb8ef112fa25d6a79582
Prashant <prashant.bhushan@outlook.com>
2022-10-22 15:25:40 +0530

    adds extractMax to MaxBinaryHeap

--- src/ds/BinaryHeap.test.ts
+++ src/ds/BinaryHeap.test.ts
@@ -1,39 +1,76 @@
 import { MaxBinaryHeap,MinBinaryHeap } from "./BinaryHeap";
 
 describe('MaxBinaryHeap: ', () => {
-    const maxBinaryHeap = new MaxBinaryHeap()
-    test('should insert a value', () => {
-        expect(maxBinaryHeap.insert(88)).toBe(0)
+    describe('insert(): ', () => {
+
+        const maxBinaryHeap = new MaxBinaryHeap()
+        test('should insert a value', () => {
+            expect(maxBinaryHeap.insert(88)).toBe(0)
+        })
+        test('should return false', () => {
+            expect(maxBinaryHeap.insert(88)).toBeUndefined()
+        })
+        test('should bubble up higher value to the top', () => {
+            expect(maxBinaryHeap.insert(95)).toBe(0)
+        })
+        test('should add to right of the parent node i.e. 95', () => {
+            expect(maxBinaryHeap.insert(5)).toBe(2)
+        })
+        test('should add to left of the parent node i.e. 88', () => {
+            expect(maxBinaryHeap.insert(15)).toBe(3)
+        })
+        test('should add to right of the parent node i.e. 88', () => {
+            expect(maxBinaryHeap.insert(25)).toBe(4)
+        })
+        test('should add to right of the parent node i.e. 95 and values[5] == 5', () => {
+            expect(maxBinaryHeap.insert(22)).toBe(2)
+            expect(maxBinaryHeap.values[5]).toBe(5)
+        })
+        test('should add to right of the parent node i.e. 95 and values[5] == 5 and values[6]== 22', () => {
+            expect(maxBinaryHeap.insert(34)).toBe(2)
+            expect(maxBinaryHeap.values[5]).toBe(5)
+            expect(maxBinaryHeap.values[6]).toBe(22)
+        })
+        test('should values[0]==102', () => {
+            expect(maxBinaryHeap.insert(104)).toBe(0)
+            expect(maxBinaryHeap.values[1]).toBe(95)
+            expect(maxBinaryHeap.values[3]).toBe(88)
+            expect(maxBinaryHeap.values[6]).toBe(22)
+        })
     })
-    test('should return false', () => {
-        expect(maxBinaryHeap.insert(88)).toBeUndefined()
-    })
-    test('should bubble up higher value to the top', () => {
-        expect(maxBinaryHeap.insert(95)).toBe(0)
-    })
-    test('should add to right of the parent node i.e. 95', () => {
-        expect(maxBinaryHeap.insert(5)).toBe(2)
-    })
-    test('should add to left of the parent node i.e. 88', () => {
-        expect(maxBinaryHeap.insert(15)).toBe(3)
-    })
-    test('should add to right of the parent node i.e. 88', () => {
-        expect(maxBinaryHeap.insert(25)).toBe(4)
-    })
-    test('should add to right of the parent node i.e. 95 and values[5] == 5', () => {
-        expect(maxBinaryHeap.insert(22)).toBe(2)
-        expect(maxBinaryHeap.values[5]).toBe(5)
-    })
-    test('should add to right of the parent node i.e. 95 and values[5] == 5 and values[6]== 22', () => {
-        expect(maxBinaryHeap.insert(34)).toBe(2)
-        expect(maxBinaryHeap.values[5]).toBe(5)
-        expect(maxBinaryHeap.values[6]).toBe(22)
-    })
-    test('should values[0]==102', () => {
-        expect(maxBinaryHeap.insert(104)).toBe(0)
-        expect(maxBinaryHeap.values[1]).toBe(95)
-        expect(maxBinaryHeap.values[3]).toBe(88)
-        expect(maxBinaryHeap.values[6]).toBe(22)
+
+    describe('extractMax()', () => {
+        const maxBinaryHeap = new MaxBinaryHeap()
+        test('should return undefined', () => {
+            expect(maxBinaryHeap.extractMax()).toBeUndefined()
+        })
+        test('should return 41', () => {
+            maxBinaryHeap.insert(41)
+            expect(maxBinaryHeap.extractMax()).toBe(41)
+            expect(maxBinaryHeap.values.length).toBe(0)
+            maxBinaryHeap.insert(41)
+            maxBinaryHeap.insert(39)
+            maxBinaryHeap.insert(33)
+            maxBinaryHeap.insert(18)
+            maxBinaryHeap.insert(27)
+            maxBinaryHeap.insert(12)
+        })
+        
+        test('should return 41',()=>{
+            expect(maxBinaryHeap.extractMax()).toBe(41)
+            expect(maxBinaryHeap.values.length).toBe(5)
+            expect(maxBinaryHeap.values[0]).toBe(39)
+            expect(maxBinaryHeap.values[1]).toBe(27)
+            expect(maxBinaryHeap.values[4]).toBe(12)
+        })
+
+        test('should return 39',()=>{
+            expect(maxBinaryHeap.extractMax()).toBe(39)
+            expect(maxBinaryHeap.values.length).toBe(4)
+            expect(maxBinaryHeap.values[2]).toBe(12)
+            expect(maxBinaryHeap.values[3]).toBe(18)
+            expect(maxBinaryHeap.values[1]).toBe(27)
+        })
     })
 })
 
--- src/ds/BinaryHeap.ts
+++ src/ds/BinaryHeap.ts
@@ -1,48 +1,79 @@
-class BinaryHeap{
-   protected _values:Array<any> = []
-
-   insert(value:any):number{
-        let values = this._values
-        
-        if(values.includes(value))return undefined
-        let childInd:number = values.push(value) - 1
-        let parentInd:number = Math.floor((childInd-1)/2)
-        
-        if(parentInd >= 0){
-            while(((this instanceof MinBinaryHeap) && values[childInd] < values[parentInd] ) ||
-            ((this instanceof MaxBinaryHeap) && values[childInd] > values[parentInd] )){
-                [values[childInd],values[parentInd]] = [values[parentInd],values[childInd]]
-                childInd = parentInd
-                parentInd = Math.floor((childInd-1)/2)
-            }
-        }
-        return childInd
-    }
+class BinaryHeap {
+  protected _values: Array<any> = []
 }
 
-class MaxBinaryHeap extends BinaryHeap{
-    constructor(){
-        super()
-    }
+class MaxBinaryHeap extends BinaryHeap {
+  constructor() {
+    super()
+  }
 
-    get values():Array<any>{
-        return this._values
+  get values(): Array<any> {
+    return this._values
+  }
+
+  insert(value: any): number {
+    let values = this._values
+    if (values.includes(value)) return undefined
+    let childInd: number = values.push(value) - 1
+    let parentInd: number = Math.floor((childInd - 1) / 2)
+    while (values[childInd] > values[parentInd]) {
+      ;[values[childInd], values[parentInd]] = [values[parentInd],values[childInd]]
+      childInd = parentInd
+      parentInd = Math.floor((childInd - 1) / 2)
     }
+    return childInd
+  }
+
+  extractMax(): any {
+    let values: Array<any> = this._values
     
-}
+    if (values.length <= 1) return values.pop();
+    [values[0], values[values.length - 1]] = [values[values.length - 1],values[0]]
+    const result = values.pop()
+    console.log(values);
+    this._sinkDown(values)
+    return result
+  }
 
+  _sinkDown(values: Array<any>) {
+    let idx: number = 0
+    while (idx < values.length) {
+      let parentIdx = idx
+      let firstChildIdx = 2 * idx + 1
+      let secondChildIdx = 2 * idx + 2
+      let nextSwap = Math.max(values[firstChildIdx], values[secondChildIdx])
+      if (!nextSwap || nextSwap < values[parentIdx] ) return
+      let nextIdx = values.indexOf(nextSwap);
+      [values[idx], values[nextIdx]] = [values[nextIdx], values[idx]]
+      idx = nextIdx
+    }
+  }
+}
 
 // MIN BINARY HEAP
-class MinBinaryHeap extends BinaryHeap{
-    constructor(){
-        super()
-    }
+class MinBinaryHeap extends BinaryHeap {
+  constructor() {
+    super()
+  }
 
-    get values():Array<any>{
-        return this._values
-    }
+  get values(): Array<any> {
+    return this._values
+  }
 
-    
+  insert(value: any): number {
+    let values = this._values
+    if (values.includes(value)) return undefined
+    let childInd: number = values.push(value) - 1
+    let parentInd: number = Math.floor((childInd - 1) / 2)
+    if (parentInd >= 0) {
+      while (values[childInd] < values[parentInd]) {
+        ;[values[childInd], values[parentInd]] = [values[parentInd],values[childInd]]
+        childInd = parentInd
+        parentInd = Math.floor((childInd - 1) / 2)
+      }
+    }
+    return childInd
+  }
 }
 
-export { MaxBinaryHeap,MinBinaryHeap }
+export { MaxBinaryHeap, MinBinaryHeap }
